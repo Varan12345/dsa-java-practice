94dc0cd2f5886c3a6b95b4d8a48960ba9e40e828
Varan singh <varansingh2409@gmail.com>
2026-05-20 16:35:16 +0530

    leetcode problem

---
+++ first_bad_version.java
@@ -0,0 +1,21 @@
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, 
+// the latest version of your product fails the quality check. Since each version is developed based 
+// on the previous version, all the versions after a bad version are also bad
+class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1; 
+            }
+        }
+
+        return left;
+    }
+}
---
+++ interaction_of_twoArrays.java
@@ -0,0 +1,26 @@
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the
+//  result must appear as many times as it shows in both arrays and you may return the result 
+// in any order.
+class Solution{
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums1) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> intersection = new ArrayList<>();
+        for (int num : nums2) {
+            if (countMap.getOrDefault(num, 0) > 0) {
+                intersection.add(num);
+                countMap.put(num, countMap.get(num) - 1);
+            }
+        }
+
+        // Convert the list to an array
+        int[] result = new int[intersection.size()];
+        for (int i = 0; i < intersection.size(); i++) {
+            result[i] = intersection.get(i);
+        }
+        return result;
+    }
+}
