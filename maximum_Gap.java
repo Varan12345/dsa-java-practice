8a84543b00da2208783c61f0583861445e09732f
Varan singh <varansingh2409@gmail.com>
2026-06-23 17:44:10 +0530

    leetcode problem

---
+++ maximum_Gap.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        int bucketSize = Math.max(1, (max - min) / (nums.length - 1));
+        int bucketCount = (max - min) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+        Arrays.fill(bucketMin, Integer.MAX_VALUE);
+        Arrays.fill(bucketMax, Integer.MIN_VALUE);
+
+        for (int num : nums) {
+            int index = (num - min) / bucketSize;
+            bucketMin[index] = Math.min(bucketMin[index], num);
+            bucketMax[index] = Math.max(bucketMax[index], num);
+        }
+
+        int maxGap = 0;
+        int previousMax = min;
+
+        for (int i = 0; i < bucketCount; i++) {
+            if (bucketMin[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            maxGap = Math.max(maxGap, bucketMin[i] - previousMax);
+            previousMax = bucketMax[i];
+        }
+
+        return maxGap;
+    }
+}
