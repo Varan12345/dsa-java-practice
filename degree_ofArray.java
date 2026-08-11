0191d01c2ea3f6fce383dff292addd4c35721b67
Varan singh <varansingh2409@gmail.com>
2026-08-11 23:14:54 +0530

    initial commit

---
+++ degree_ofArray.java
@@ -0,0 +1,27 @@
+class Solution{
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (!left.containsKey(num)) {
+                left.put(num, i);
+            }
+            right.put(num, i);
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        int degree = Collections.max(count.values());
+        int minLength = nums.length;
+
+        for (int num : count.keySet()) {
+            if (count.get(num) == degree) {
+                minLength = Math.min(minLength, right.get(num) - left.get(num) + 1);
+            }
+        }
+
+        return minLength;
+    }
+}
