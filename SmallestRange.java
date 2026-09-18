09ee902fbd252036f1fbd2610f413eb6c854ebc0
Varan singh <varansingh2409@gmail.com>
2026-09-18 23:42:58 +0530

    initial commit

---
+++ SmallestRange.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int smallestRangeII(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int minRange = nums[n - 1] - nums[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            int high = Math.max(nums[i] + k, nums[n - 1] - k);
+            int low = Math.min(nums[0] + k, nums[i + 1] - k);
+            minRange = Math.min(minRange, high - low);
+        }
+
+        return minRange;
+    }
+}
